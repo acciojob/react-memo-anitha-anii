a03148c11bea1fc0cfaed39eff7e159bf0c77d12
anitha-anii <anithachowdary355@gmail.com>
2023-07-24 15:56:23 +0530

    'changes'

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,58 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
 function MemoApp() {
-  const [count, setCount] = useState(0);
-  const [memoText, setMemoText] = useState("");
-  const [memoItems, setMemoItems] = useState([]);
-  const [memoItems2, setMemoItems2] = useState([]);
-  function handleAddTodo() {
-    setMemoItems([...memoItems, "New todo"]);
-  }
-
-  function handleIncrement() {
-    setCount((prevCount) => prevCount + 1);
-  }
-
-  function handleInputChange(event) {
-    setMemoText(event.target.value);
-  }
-
-  function handleSubmit() {
-    if (memoText!=='') {
-      setMemoItems2([...memoItems2, memoText]);
-      setMemoText("");
-    }
-  }
-
-  return (
-    <div id="main">
-      <h2>Memo</h2>
-      <button id="add-todo-btn" onClick={handleAddTodo}>Add todo</button>
-      {memoItems.map((todo, index) => (
-         <p key={index}>{todo}</p>
-      ))}
-      <p>Count: {count}</p>
-      <button id="calc" onClick={handleIncrement}>0</button>
-      <br />
-      <br />
-      <input
-        type="text"
-        value={memoText}
-        onChange={handleInputChange}
-       
-        id="skill-input" 
-      />
-      <button id="skill-btn" onClick={handleSubmit}>Submit</button>
-      <br />
-      <br />
+  
+      const [todos, setTodos] = useState([]);
+      const [count, setCount] = useState(0);
+      const [inputText, setInputText] = useState('');
+    
+           useEffect(() => {
+        document.title = `Count: ${count}`;
+      }, [count]);
+    
+     
+      const handleAddTodo = () => {
+        setTodos(prevTodos => [...prevTodos, { id: todos.length + 1, content: 'New todo' }]);
+      };
+    
+           const handleIncrement = () => {
+        setCount(prevCount => prevCount + 1);
+      };
+    
+           const handleInputChange = event => {
+        setInputText(event.target.value);
+      };
+    
+           const handleAddItem = () => {
+        if (inputText.length > 5) {
+          setTodos(prevTodos => [...prevTodos, { id: todos.length + 1, content: inputText }]);
+          setInputText('');
+        }
+      };
+    
+      return (
+        <div id="main">
+          <h2 id="item-jumbotron">Todo List</h2>
+          <ul id="todo-0">
+            {todos.map(todo => (
+              <li key={todo.id}>{todo.content}</li>
+            ))}
+          </ul>
+    
+          <button id="add-todo-btn" onClick={handleAddTodo}>
+            Add Todo
+          </button>
+    
+          <div id="incr-cnt">
+            <p>Count: {count}</p>
+            <button onClick={handleIncrement}>0</button>
+          </div>
+    
+          <div id="calc">
+            <input
+              id="skill-input"
+              type="text"
+              value={inputText}
+              onChange={handleInputChange}
+              placeholder="Enter text (min. 5 characters)"
+            />
+            <button id="skill-btn" onClick={handleAddItem}>
+              Submit
+            </button>
+          </div>
+        </div>
+      );
+    };
       
-      <ul>
-        {memoItems2.map((item, index) => (
-          <li key={index} id={`todo-${index}`} >{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
+  
 export default MemoApp;
